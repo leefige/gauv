62c292554b44bc607bd3e87a63878698e193f1d7
Yifei Li <liyifei19@mails.tsinghua.edu.cn>
2021-10-22 17:27:30 +0800

    WIP: one party one channel.

--- src/mpclib/context.hpp
+++ src/mpclib/context.hpp
@@ -14,26 +14,39 @@ namespace mpc {
 /* NOT thread-safe! */
 template<base_t BASE>
 class mpc_context {
-    using F = field<BASE>;
-    using P = par<BASE>;
-    using S = share<BASE>;
-
-    mpc_context() noexcept = default;
+    using P_T = par<BASE>;
+    using PS_T = parset<BASE>;
+    using S_T = share<BASE>;
 
     mpc_context(const mpc_context&) = delete;
     mpc_context(mpc_context&&) = delete;
     mpc_context& operator=(const mpc_context&) = delete;
     mpc_context& operator=(mpc_context&&) = delete;
 
-public:
     struct party_queue {
-        std::reference_wrapper<const P> party;
-        std::deque<S> queue;
-        party_queue(const P& p) : party(p), queue() {}
+        std::reference_wrapper<const P_T> party;
+        std::unordered_map<uint32_t, std::deque<S_T>> queues;
+        party_queue(const P_T& p, const PS_T& ps) : party(p)
+        {
+            // insert a queue for each party: private channels
+            for (const P_T& pj : ps) {
+                queues.insert(std::make_pair(pj.hash(), std::deque<S_T>()));
+            }
+        }
     };
 
-private:
+    const PS_T _parties;
     std::unordered_map<uint32_t, party_queue> _msgs;
+
+    mpc_context(const PS_T& parties) noexcept : _parties(parties)
+    {
+        for (const P_T& p : _parties) {
+            if (_msgs.find(p.hash()) != _msgs.end()) {
+                throw party_duplicated<BASE>(*(_msgs.find(p.hash())), p);
+            }
+            register_party(p);
+        }
+    }
 
 public:
     static std::shared_ptr<mpc_context<BASE>> get_context()
@@ -42,13 +55,7 @@ public:
         return singleton;
     }
 
-    void register_party(P& party)
-    {
-        _msgs.insert(std::make_pair(party.hash(), party_queue(party)));
-        party.register_context(get_context());
-    }
-
-    void send(const P& sender, const P& receiver, const S& msg)
+    void send(const P_T& sender, const P_T& receiver, const S_T& msg)
     {
         if (receiver != msg.party()) {
             throw party_missmatch(receiver, msg.party());
@@ -61,7 +68,7 @@ public:
         _msgs.at(receiver.hash()).queue.push_back(msg);
     }
 
-    std::optional<S> receive(const P& receiver)
+    std::optional<S_T> receive(const P_T& receiver)
     {
         if (_msgs.find(receiver.hash()) == _msgs.end()) {
             throw party_nonexist();
@@ -75,6 +82,13 @@ public:
         auto& ret = dst.front();
         dst.pop_front();
         return ret;
+    }
+
+private:
+    void register_party(P_T& party)
+    {
+        _msgs.insert(std::make_pair(party.hash(), party_queue(party, _parties)));
+        party.register_context(get_context());
     }
 };
 
--- src/mpclib/mpc_exceptions.hpp
+++ src/mpclib/mpc_exceptions.hpp
@@ -56,6 +56,22 @@ public:
 };
 
 template<base_t BASE>
+class party_duplicated: public std::exception {
+    std::string msg;
+public:
+    party_duplicated(const par<BASE>& old_one, const par<BASE>& new_one) noexcept
+    {
+        std::stringstream ss;
+        ss << "Party hash code collision: " << old_one << " and " << new_one;
+        msg = ss.str();
+    }
+    virtual const char* what() const noexcept override
+    {
+        return msg.c_str();
+    }
+};
+
+template<base_t BASE>
 class empty_message_queue: public std::exception {
     std::string msg;
 public:
