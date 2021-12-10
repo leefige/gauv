5fe062c2615c3b6bd990f2bf0e251971ee8d7a6d
Yifei Li <liyifei19@mails.tsinghua.edu.cn>
2021-12-10 13:15:45 +0800

    Overload ss for operator.

--- src/mpcgraph/operator.hpp
+++ src/mpcgraph/operator.hpp
@@ -61,6 +61,44 @@ std::ostream& operator<<(std::ostream& o, const Operator& op)
     return o;
 }
 
+std::stringstream& operator<<(std::stringstream& o, const Operator& op)
+{
+    switch (op) {
+    case Operator::NONE:
+        o << "NONE";
+        break;
+    case Operator::INPUT:
+        o << "INPUT";
+        break;
+    case Operator::TRANSFER:
+        o << "TRANSFER";
+        break;
+    case Operator::ADD:
+        o << "ADD";
+        break;
+    case Operator::SUB:
+        o << "SUB";
+        break;
+    case Operator::MUL:
+        o << "MUL";
+        break;
+    case Operator::DIV:
+        o << "DIV";
+        break;
+    case Operator::EVAL:
+        o << "EVAL";
+        break;
+    case Operator::RECONSTRUCT:
+        o << "RECONSTRUCT";
+        break;
+
+    default:
+        o << "<UNKNOWN_OP>";
+        break;
+    }
+    return o;
+}
+
 class Equation {
     const Operator _op;
     std::vector<Expression*> _oprands;
