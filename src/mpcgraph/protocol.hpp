0830bd4b2b18c270f318dea8da23600012bf8cf8
Yifei Li <liyifei19@mails.tsinghua.edu.cn>
2021-12-24 23:41:10 +0800

    Prototype for protocol.

---
+++ src/mpcgraph/protocol.hpp
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "builtin.hpp"
+#include "dag.hpp"
+
+#include <ostream>
+
+namespace mpc {
+
+class Protocol {
+public:
+    Protocol() noexcept
+    {
+
+    }
+
+    void compile()
+    {
+
+    }
+
+};
+
+} /* namespace mpc */
